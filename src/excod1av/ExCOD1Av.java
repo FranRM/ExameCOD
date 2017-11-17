b12fe1ca4120fe7da2e845d0da36ca1dc2865818
femio23 <chiscazo.paco@gmail.com>
2017-11-17 09:55:20 +0100

    Correccion. Commit C.

--- src/excod1av/ExCOD1Av.java
+++ src/excod1av/ExCOD1Av.java
@@ -13,7 +13,7 @@ public class ExCOD1Av {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
         
-        System.out.println("Benvido ó exame");
+        System.out.println("Benvido ó exame, preparese.");
     }
     
 }
